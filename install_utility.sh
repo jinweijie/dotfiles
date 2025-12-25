395a0f3b28cd5ee31d7dc9311a71aaaf3bc1415a
JIN Weijie <jinweijie@gmail.com>
2025-12-25 12:38:04 +0000

    Update install utility script

--- install_utility.sh
+++ install_utility.sh
@@ -10,6 +10,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+rm -rf lazygit.tar.gz lazygit
 
 # lazydocker
 LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
