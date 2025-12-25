c704706f76829dd1795b79e21aa0188b2b8b26bf
JIN Weijie <jinweijie@gmail.com>
2025-12-25 11:16:24 +0800

    Add install utility

---
+++ install_utility.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install gdu
+curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
+chmod +x gdu_linux_amd64
+sudo mv gdu_linux_amd64 /usr/bin/gdu
+
+# layzygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+# lazydocker
+LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+mkdir lazydocker-temp
+tar xf lazydocker.tar.gz -C lazydocker-temp
+sudo mv lazydocker-temp/lazydocker /usr/local/bin
+rm -rf lazydocker.tar.gz lazydocker-temp
+
+# yazi
+# wget -qO yazi.zip https://github.com/sxyazi/yazi/releases/latest/download/yazi-x86_64-unknown-linux-gnu.zip
+# unzip -q yazi.zip -d yazi-temp
+# sudo mv yazi-temp/*/{ya,yazi} /usr/local/bin
+# rm -rf yazi-temp yazi.zip
